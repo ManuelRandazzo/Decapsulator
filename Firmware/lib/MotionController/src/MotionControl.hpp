39403f3c0ceebcd9e3021abc3a56fab7b7313c36
Manuel Randazzo <randazzomanuel05@gmail.com>
2026-01-30 00:31:07 +0100

    organization(Motion Control): diviso la parte di sviluppo dei metodi dalla definizione della classe per leggibilità, spostato i file in lib/MotionControl

---
+++ Firmware/lib/MotionController/src/MotionControl.hpp
@@ -0,0 +1,249 @@
+#pragma once
+// -*- Gruppo 1-FreeRTOS-C++
+
+/*
+ * This file is part of {{ Decapsulator }}.
+ *
+ * Developed for the ITS Meccatronico Montebelluna-Castelfranco Veneto 2024-2026 Gruppo 1.
+ * This product includes software developed by the Gruppo 1 Project.
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is NOT free software: you can NOT redistribute it and/or modify it.
+ *
+ * The Documentation is made following the Doxygen standard documentation rules
+ *
+ */
+
+
+/**
+ *
+ * @brief QUESTO FILE CONTIENE LA CLASSE PER L'HANDLING DEI MOTORI STEPPER
+ *
+ */
+#include "freertos/portmacro.h"
+#include "DRV8825_Decapsulator.hpp" /// Driver dello stepper
+//#include "PID.h"   /// Classe per la stabilità del motore
+#include "Tasks.hpp"   /// Creazione delle tasks
+#include "Interrupts.hpp"
+#include "Debug.hpp"
+
+/*-----------------------------------------------
+  |                                             |
+  |           CLASS USER ENUMERATIONS           |          
+  |                                             |
+  -----------------------------------------------*/
+
+
+/// @enum per il numero di microsteps
+typedef enum : uint8_t
+{
+  FULL_STEP    = 1,  // fa uno step intero per ogni segnale sul pin STEP
+  STEP_1_TO_2  = 2,  // fa 1/2 passo per ogni segnale sul pin STEP
+  STEP_1_TO_4  = 4,  // fa 1/4 di passo per ogni segnale sul pin STEP
+  STEP_1_TO_8  = 8,  // fa 1/8 di passo per ogni segnale sul pin STEP
+  STEP_1_TO_16 = 16, // fa 1/16 di passo per ogni segnale sul pin STEP
+  STEP_1_TO_32 = 32, // fa 1/32 di passo per ogni segnale sul pin STEP
+} uSteps_t;
+
+
+
+/// @enum per l'OUTPUT del finecorsa
+typedef enum : uint8_t
+{
+  ACTIVE_LOW = LOW,
+  ACTIVE_HIGH = HIGH,
+} CalibSignal_t;
+
+
+
+/// @enum per la direzione del motore
+typedef enum : int8_t
+{
+  DIR_NEGATIVE = DRV8825_COUNTERCLOCK_WISE,
+  DIR_POSITIVE = DRV8825_CLOCK_WISE,
+} Direction_t;
+
+
+
+/// @enum per sapere se il motore allo STOP rimane in coppia o no
+typedef enum : uint8_t
+{
+  RELEASE = 0,
+  HOLD    = 1,
+} StopReleaseOrHold_t;
+
+
+
+/*-----------------------------------------------
+  |                                             |
+  |               CLASS DECLARATION             |          
+  |                                             |
+  -----------------------------------------------*/
+
+
+
+
+/**
+ *
+ *  @warning ATTENZIONE: I PIN "MODE 0, MODE 1, MODE 2" VENGONO DECISI DALLA CONFIGURAZIONE HARDWARE DIRETTAMENTE NEL PCB PER IL MICROSTEPPING
+ *                       TABELLA DI @ref SUL DATASHEET.
+ *  @warning ATTENZIONE: IL PIN DECAY FORSE SAREBBE MEGLIO LASCIARLO FLOATTANTE PER AVERE UN MIX DI PRECISIONE E SILENZIOSITA'
+ *
+ */
+
+class MOTION : private DRV8825
+{
+  public :
+    /// Costruttore
+    MOTION() {};
+
+    /// Distruttore
+    ~MOTION();
+
+    /// Inizializza il Motion Controller
+    inline drv_err_t Init(uint16_t numberOfSteps, uint8_t dir_pin, uint8_t step_pin, uint8_t en_pin, uint8_t rst_pin, uint8_t sleep_pin,
+                          UBaseType_t taskPriority, uSteps_t microSteps, uint8_t fault_pin = 255, void (*FaultISR)() = nullptr, uint32_t FaultISR_Heap = 4096, UBaseType_t FaultISR_priority = 15);
+
+    /// Mette in coppia il motore
+    inline void attach();    
+
+    /// Disaccoppia il motore
+    inline void detach();
+
+    /// Restituisce se il motore è attached (true) o no (false)
+    inline bool isAttached();
+
+    /// Restituisce se il motore è detached (true) o no (false) 
+    inline bool isDetached();
+
+    /// Inizializza il motore con l'Homing in modo che si sappia il punto di partenza
+    inline void home(uint8_t calibrationPin, uint8_t inputModePin, uint8_t triggerMode, double HomeVelocity_gradi_sec, Direction_t searchDirection,
+                     double gradiDopoHome, uint8_t quanteVolteToccaIlSensore, CalibSignal_t calibCamSignal);
+
+    /// @return se è finito(true) o no(false) l'homing
+    inline bool isHomeDone();
+
+    /// Permette al motore di avviarsi e muoversi, almeno una volta deve essere chiamata questa funzione
+    inline void Start();
+
+    /// Ferma il motore lasciandolo in coppia o togliendo corrente e necessita di un nuovo Start()
+    inline void Stop(StopReleaseOrHold_t rilasciaOppureMantieniCoppia = RELEASE);
+
+    /// Ferma il motore lasciandolo in coppia non serve richiamare un nuovo Start()
+    inline void Halt();
+
+    /// @return true se il motore è fermo e non può essere comandato
+    inline bool isStopped();
+
+    /// @return true se il motore può essere comandato
+    inline bool isStarted();
+
+    /// Muove il motore in una direzione e alla velocità specificata in modo RELATIVO
+    inline void moveRel(double gradi, double speed_gradi_al_secondo = 0.0);
+
+    /// Muove il motore in una direzione e alla velocità specificata in modo ASSOLUTO rispetto all'accensione
+    inline void moveAbs(double gradi, double speed_gradi_al_secondo = 0.0);
+
+    /// Muove il motore all'infinito verso la direzione specificata
+    inline void moveContinuous(Direction_t direzione, double speed_gradi_al_secondo = 0.0);
+
+    /// Restituisce se il movimento è finito o no così da poterne iniziare un altro
+    inline bool isStepDone();
+
+    /// Driver in low power mode, Disaccoppia il motore, Ignora TUTTI gli Input, spegne : clock, pompa di carica, regolatore interno 
+    /// Se state = FALSE è "sveglio", se state = TRUE allora va in sleep mode, se è già spento o già acceso e viene ripetuta l'operazione non fa nulla
+    inline void sleep(bool state = true);
+
+    ///resetta il driver e le variabili
+    inline void reset();
+
+    /// Converte da gradi a steps con segno
+    inline int64_t gradiToSteps(double gradi);
+
+    /// Converte da steps a gradi con segno
+    inline double stepsToGradi(int64_t steps);
+
+    /// Definizione di una Interrupt Service Routine (ISR) relativa al pin nFAULT del DRV8825 per monitoraggio asincrono.
+    inline void setFaultISR(uint8_t fault_pin, void (*FaultISR)(), uint32_t FaultISR_Heap = 4096, UBaseType_t priority = 15);
+
+    /// @return la posizione assoluta in steps
+    inline double getPosition();
+
+    /// @return la posizione assoluta in steps
+    inline int64_t getPositionInSteps();
+
+  private : /// Dato che la libreria del driver fornisce come protected delle variabili la classe MOTION le eredita
+    DRV8825 Motion; /// Oggetto del driver usato per il motore
+
+    typedef enum : uint8_t { STAND_STILL, HOMING, MOVE_REL, MOVE_ABS, CONTINUOUS } SwitchMove_t;
+    
+    #ifdef LOG_ACTIVE_MOTION
+      const char* SwitchMoveStr[5] = { "STAND STILL", "HOMING", "MOVE RELATIVE", "MOVE ABSOLUTE", "MOVE CONTINUOUS" };
+    #endif
+
+    int64_t absoluteStepCounter;                /*!< Variabile di quanti step ha fatto il motore dall'accensione  */
+
+    bool __isHomeFinished = false;              /*!< indica se l'homing è finito o no  */
+
+    INTERRUPTS nFAULT_ISR;                      /*!< Oggetto della Classe della gestione degli Interrupts */
+    
+    void (*__FaultISR)();                       /*!< Funzione che viene chiamata all'interno della ISR dell'oggetto nFAULT_ISR*/
+
+    uSteps_t uStepScelti;                       /*!< Microsteps scelti con la config. HW con pin MODE_0 - MODE_2  */
+    
+    uint16_t __stepsMotore;                     /*!< Si salva quanti step/giro ha il motore (Es. 200 step/giro)  */
+
+    QueueHandle_t MoveQueueHandler;             /*!< E' l'Handler della coda usata per bufferizzare i comandi di movimento  */
+    typedef struct xQueueMoveDataStruct
+    {
+      /// ID dello stato dell'azione chiamante
+      SwitchMove_t __SwitchMove;                /*!< Variabile switch per il movimento del motore nella task  */
+
+      /// Dati Homing
+      uint64_t __home_steps_us;                 /*!< Velocità dell'homing in step/secondo  */
+      unsigned __calibPin : 6;                  /*!< indica il pin di calibrazione (finecorsa), 6 bits = 64pin max  */
+      unsigned __calibSig : 1;                  /*!< è il valore che assume il sensore quando viene attivato  */
+      uint8_t __nCalibTouch : 4;                /*!< indica quante volte viene toccato il sensore per far sì che sia calibrato  */
+      int64_t __absPostHomeVal;                 /*!< è il valore assoluto che viene associato dopo l'homing  */
+
+      /// Altri Dati
+      uint64_t __speed_steps_us;                /*!< Velocità step/secondo  */
+      int64_t __move_steps;                     /*!< Passi da eseguire scelti in runtime  */
+      Direction_t __dir;                        /*!< Direzione che verrà impostata all'invio del comando  */  
+    } MoveQueue_t;
+
+ 
+    MoveQueue_t receiverQueue;                  /*!< Struct che contiene gli attuali dati ricevuti  */
+
+    BaseType_t MoveSendToQueue(MoveQueue_t StructToSend);
+
+    //inline int64_t updateStepsPosition();       /*!< Aggiorna la posizione e ritorna anche la posizione assoluta  */
+
+    TaskTypeDef MoveHandlerTask;                /*!< Oggetto alla classe delle tasks  */
+    
+    void MoveHandler();                         /*!< Funzione che esegue la task  */
+
+    INTERRUPTS HOME_IT;                         /*!< Definisce l'oggetto di homing  */
+   
+    /// Funzione per gestire l'interruzione da parte del sensore di calibrazione, 
+    /// non viene più eseguita quando finisce l'homing e non viene più chiamato l'homing
+    void HomingReachedISR();
+
+    /// Funzione per ottenere 1 se il segnale della camma è attivo oppure 0 se non è attivo
+    inline bool getCamSignal(); 
+
+    /// Funzione per ottenere il delay per poter cambiare la velocità del movimento
+    inline uint64_t getPeriodDelay(const double gradiSecondo);   
+    
+    SwitchMove_t selettore = STAND_STILL;
+
+    bool __isStopped = true;                  /*!< Flag di motore stoppato o avviato modificato da Start() e Stop() e restituito da
+                                                   isStopped e isStarted  */
+    
+    bool __isHalted = false;                   /*!< Flag di motore in Halt modificato da Halt e tutte le azioni di movimento  */
+
+    bool __isAttached = false;                /*!< Flag di motore stoppato o avviato modificato da Start() e Stop() e restituito da
+                                                   isStopped e isStarted  */
+};
+
