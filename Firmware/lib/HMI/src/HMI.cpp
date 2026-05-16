9446d0d2c8e5833f10b8c5310b141dd79c4b1e22
Manuel Randazzo <randazzomanuel05@gmail.com>
2026-05-16 14:42:31 +0200

    add(programma HMI) : aggiunto il programma di gestione della Human Machine Interface generato da SquareLineStudio nella task dell'HMI

--- Firmware/lib/HMI/src/HMI.cpp
+++ Firmware/lib/HMI/src/HMI.cpp
@@ -15,13 +15,108 @@
 
 #include "HMI.hpp"
 
+#ifndef LV_COLOR_DEPTH
+#error "LV_COLOR_DEPTH is not defined!"
+#endif
+
+#if LV_COLOR_DEPTH != 16
+#error "This driver only supports LV_COLOR_DEPTH 16 (RGB565)"
+#endif
+
+TFT_eSPI tft = TFT_eSPI();
+
+static const uint16_t screenWidth = 480;
+static const uint16_t screenHeight = 320;
+
+enum BoardConstants { LVGL_BUFFER_RATIO = 10 };
+enum { SCREENBUFFER_SIZE_PIXELS = screenWidth * screenHeight / BoardConstants::LVGL_BUFFER_RATIO };
+
+static uint16_t buf[SCREENBUFFER_SIZE_PIXELS] __attribute__((aligned(4)));
+
+/* -------------------------------------------------------------------------- */
+/*  LVGL logging (optional)                                                    */
+/* -------------------------------------------------------------------------- */
+
+#if LV_USE_LOG != 0
+void my_print(const char *buf) {
+  Serial.printf(buf);
+  Serial.flush();
+}
+#endif
+
+/* -------------------------------------------------------------------------- */
+/*  Display flush callback                                                     */
+/* -------------------------------------------------------------------------- */
+
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *pixelmap) {
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.pushColors((uint16_t *)pixelmap, w * h, true);
+  tft.endWrite();
+
+  lv_disp_flush_ready(disp);
+}
+
+/* -------------------------------------------------------------------------- */
+/*  Touchpad callback                                                          */
+/* -------------------------------------------------------------------------- */
+
+void my_touchpad_read(lv_indev_t *indev_driver, lv_indev_data_t *data) {
+  uint16_t touchX = 0, touchY = 0;
+
+  bool touched = false;  // tft.getTouch(&touchX, &touchY, 600);
+
+  if (!touched) {
+    data->state = LV_INDEV_STATE_REL;
+  } else {
+    data->state = LV_INDEV_STATE_PR;
+    data->point.x = touchX;
+    data->point.y = touchY;
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+/*  LVGL tick callback                                                         */
+/* -------------------------------------------------------------------------- */
+
+static uint32_t my_tick_get_cb(void) { return millis(); }
+
+
 void task(void* pvParameters)
 {
-    /// @todo Metti Qua il contenuto di
-    ///       void setup() nel @file ui.ino
+    lv_init();
+
+    #if LV_USE_LOG != 0
+        lv_log_register_print_cb(my_print);
+    #endif
+
+    tft.begin();
+    tft.setRotation(3);  // Landscape orientation
+
+    static lv_display_t *disp;
+    disp = lv_display_create(screenWidth, screenHeight);
+    lv_display_set_color_format(disp, LV_COLOR_FORMAT_RGB565);
+    lv_display_set_buffers(disp, buf, NULL, sizeof(buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+
+    static lv_indev_t *indev;
+    indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    lv_tick_set_cb(my_tick_get_cb);
+
+    ui_init();
+
+    Serial.println("Setup done");
+
+
     while(1)
     {
-        /// @todo Metti Qua il contenuto di
-        ///       void loop() nel @file ui.ino 
+        lv_timer_handler();  // LVGL task handler
+        delay(5);
     }
 }
--- Firmware/lib/HMI/src/HMI.hpp
+++ Firmware/lib/HMI/src/HMI.hpp
@@ -18,9 +18,64 @@
  */
 
 
+// Arduino + LVGL 9.3 + TFT_eSPI
+
 /// Libreria per la creazione della Human Machine Interface (HMI) 
 /// @link_per_il_setup_della_libreria: https://docs.lvgl.io/master/details/integration/frameworks/platformio.html
-//#include "lvgl.h" 
-#ifdef LV_USE_TFT_ESPI
-  #include <TFT_eSPI.h>
+#include <TFT_eSPI.h>
+#include <lvgl.h>
+#include <ui.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined __has_include
+#if __has_include("lvgl.h")
+#include "lvgl.h"
+#elif __has_include("lvgl/lvgl.h")
+#include "lvgl/lvgl.h"
+#else
+#include "lvgl.h"
+#endif
+#else
+#include "lvgl.h"
+#endif
+
+#include "ui_helpers.h"
+#include "ui_events.h"
+#include "ui_theme_manager.h"
+#include "ui_themes.h"
+
+///////////////////// SCREENS ////////////////////
+
+#include "ui_Scheramata_Avvio.h"
+#include "ui_Schermata_Principale.h"
+#include "ui_Schermata_Diagnostica.h"
+#include "ui_password.h"
+#include "ui_EASTER_EGG.h"
+#include "ui_Logger1.h"
+#include "ui_JOGGER.h"
+
+///////////////////// VARIABLES ////////////////////
+
+extern lv_anim_t * glow1_Animation(lv_obj_t * TargetObject, int delay);
+
+// EVENTS
+
+extern lv_obj_t * ui____initial_actions0;
+
+// IMAGES AND IMAGE SETS
+LV_IMG_DECLARE(ui_img_1415156368);    // assets/ITSMeccatroniconew2-grigio (1).png
+LV_IMG_DECLARE(ui_img_foto_sfondo_resized_png);    // assets/foto sfondo_resized.png
+LV_IMG_DECLARE(ui_img_1414324677);    // assets/pngegg (1).png
+LV_IMG_DECLARE(ui_img_logo_decapsulator_resized_png);    // assets/logo_decapsulator_resized.png
+
+// UI INIT
+void ui_init(void);
+void ui_destroy(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
 #endif
