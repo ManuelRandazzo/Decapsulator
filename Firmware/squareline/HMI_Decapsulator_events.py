c4226f70991dce79e67eddba533a754b81290aaf
Pisnaco <pisanalessio2003@gmail.com>
2026-05-25 17:03:08 +0200

    Update ui

--- Firmware/squareline/HMI_Decapsulator_events.py
+++ Firmware/squareline/HMI_Decapsulator_events.py
@@ -6,3 +6,27 @@ def avvio_macchina(event_struct):
 def Stop_Macchina(event_struct):
     return
 
+
+def verifica_password(event_struct):
+    return
+
+
+def enable_motore_punzone(event_struct):
+    return
+
+
+def disable_motore_punzone(event_struct):
+    return
+
+
+def jog_negative(event_struct):
+    return
+
+
+def Jog_Punz_Negative(event_struct):
+    return
+
+
+def Jog_Punz_Positive(event_struct):
+    return
+
