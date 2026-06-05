a5cb438d18a7679707edb7a5600f26bb59336f80
Manuel Randazzo <randazzomanuel05@gmail.com>
2026-06-05 02:26:46 +0200

    add(ui_vars_mutexs.c) : aggiunto il file in cui sono dichiarati i mutex che verranno esternati dal .h.
    fix(extern C) : risolto il problema della funzione ui_init_var_mutexs() che non riusciva ad essere esternata perchè il file è .c quindi bisogna fare il controllo e vedere se è definito __cplusplus

---
+++ Firmware/src/HMI_UI_EEZ/ui_vars_mutexs.c
@@ -0,0 +1,160 @@
+#include "HMI_UI_EEZ/ui_vars_mutexs.h"
+
+SemaphoreHandle_t mutex_contatore_caps_totali = NULL;
+
+SemaphoreHandle_t mutex_contatore_caps_ses = NULL;
+
+SemaphoreHandle_t mutex_stato_avvio_macchina = NULL;
+
+SemaphoreHandle_t mutex_comando_macchina = NULL;
+
+SemaphoreHandle_t mutex_password_corretta = NULL;
+
+SemaphoreHandle_t mutex_str_logger_txt = NULL;
+
+SemaphoreHandle_t mutex_comando_motore_ralla = NULL;
+
+SemaphoreHandle_t mutex_comando_motore_punzone = NULL;
+
+SemaphoreHandle_t mutex_comando_avanti_motore_punzone = NULL;
+
+SemaphoreHandle_t mutex_comando_indietro_motore_punzone = NULL;
+
+SemaphoreHandle_t mutex_comando_avanti_motore_ralla = NULL;
+
+SemaphoreHandle_t mutex_comando_indietro_motore_ralla = NULL;
+
+SemaphoreHandle_t mutex_stato_motore_punzone = NULL;
+
+SemaphoreHandle_t mutex_speed_motore_punz = NULL;
+
+SemaphoreHandle_t mutex_stato_motore_ralla = NULL;
+
+SemaphoreHandle_t mutex_gradi_per_click_ralla = NULL;
+
+SemaphoreHandle_t mutex_gradi_per_click_punz = NULL;
+
+SemaphoreHandle_t mutex_speed_motore_ralla = NULL;
+
+SemaphoreHandle_t mutex_stato_sensore_di_calibrazione = NULL;
+
+SemaphoreHandle_t mutex_stato_finecorsa_max = NULL;
+
+SemaphoreHandle_t mutex_stato_finecorsa_min = NULL;
+
+SemaphoreHandle_t mutex_homing = NULL;
+
+bool ui_init_var_mutexs()
+{
+    mutex_contatore_caps_totali = xSemaphoreCreateMutex();
+    if(mutex_contatore_caps_totali == NULL)
+        return false;
+
+
+    mutex_contatore_caps_ses = xSemaphoreCreateMutex();
+    if(mutex_contatore_caps_ses == NULL)
+        return false;
+
+
+    mutex_stato_avvio_macchina = xSemaphoreCreateMutex();
+    if(mutex_stato_avvio_macchina == NULL)
+        return false;
+
+
+    mutex_comando_macchina = xSemaphoreCreateMutex();
+    if(mutex_comando_macchina == NULL)
+        return false;
+
+
+    mutex_password_corretta = xSemaphoreCreateMutex();
+    if(mutex_password_corretta == NULL)
+        return false;
+
+
+    mutex_str_logger_txt = xSemaphoreCreateMutex();
+    if(mutex_str_logger_txt == NULL)
+        return false;
+
+
+    mutex_comando_motore_ralla = xSemaphoreCreateMutex();
+    if(mutex_comando_motore_ralla == NULL)
+        return false;
+
+
+    mutex_comando_motore_punzone = xSemaphoreCreateMutex();
+    if(mutex_comando_motore_punzone == NULL)
+        return false;
+
+
+    mutex_comando_avanti_motore_punzone = xSemaphoreCreateMutex();
+    if(mutex_comando_avanti_motore_punzone == NULL)
+        return false;
+
+
+    mutex_comando_indietro_motore_punzone = xSemaphoreCreateMutex();
+    if(mutex_comando_indietro_motore_punzone == NULL)
+        return false;
+
+
+    mutex_comando_avanti_motore_ralla = xSemaphoreCreateMutex();
+    if(mutex_comando_avanti_motore_ralla == NULL)
+        return false;
+
+
+    mutex_comando_indietro_motore_ralla = xSemaphoreCreateMutex();
+    if(mutex_comando_indietro_motore_ralla == NULL)
+        return false;
+
+
+    mutex_stato_motore_punzone = xSemaphoreCreateMutex();
+    if(mutex_stato_motore_punzone == NULL)
+        return false;
+
+
+    mutex_speed_motore_punz = xSemaphoreCreateMutex();
+    if(mutex_speed_motore_punz == NULL)
+        return false;
+
+
+    mutex_stato_motore_ralla = xSemaphoreCreateMutex();
+    if(mutex_stato_motore_ralla == NULL)
+        return false;
+
+
+    mutex_gradi_per_click_ralla = xSemaphoreCreateMutex();
+    if(mutex_gradi_per_click_ralla == NULL)
+        return false;
+
+
+    mutex_gradi_per_click_punz = xSemaphoreCreateMutex();
+    if(mutex_gradi_per_click_punz == NULL)
+        return false;
+
+
+    mutex_speed_motore_ralla = xSemaphoreCreateMutex();
+    if(mutex_speed_motore_ralla == NULL)
+        return false;
+
+
+    mutex_stato_sensore_di_calibrazione = xSemaphoreCreateMutex();
+    if(mutex_stato_sensore_di_calibrazione == NULL)
+        return false;
+
+
+    mutex_stato_finecorsa_max = xSemaphoreCreateMutex();
+    if(mutex_stato_finecorsa_max == NULL)
+        return false;
+
+
+    mutex_stato_finecorsa_min = xSemaphoreCreateMutex();
+    if(mutex_stato_finecorsa_min == NULL)
+        return false;
+
+
+    mutex_homing = xSemaphoreCreateMutex();
+    if(mutex_homing == NULL)
+        return false;
+
+        
+    return true;
+}
--- Firmware/src/HMI_UI_EEZ/ui_vars_mutexs.h
+++ Firmware/src/HMI_UI_EEZ/ui_vars_mutexs.h
@@ -8,163 +8,59 @@
 /// Tempo massimo in cui i mutex bloccano se non hanno risposta
 #define MAX_MUTEX_BLOCK_TIME_TICKS pdMS_TO_TICKS(100)
 
-SemaphoreHandle_t mutex_contatore_caps_totali = nullptr;
+extern SemaphoreHandle_t mutex_contatore_caps_totali;
 
-SemaphoreHandle_t mutex_contatore_caps_ses = nullptr;
+extern SemaphoreHandle_t mutex_contatore_caps_ses;
 
-SemaphoreHandle_t mutex_stato_avvio_macchina = nullptr;
+extern SemaphoreHandle_t mutex_stato_avvio_macchina;
 
-SemaphoreHandle_t mutex_comando_macchina = nullptr;
+extern SemaphoreHandle_t mutex_comando_macchina;
 
-SemaphoreHandle_t mutex_password_corretta = nullptr;
+extern SemaphoreHandle_t mutex_password_corretta;
 
-SemaphoreHandle_t mutex_str_logger_txt = nullptr;
+extern SemaphoreHandle_t mutex_str_logger_txt;
 
-SemaphoreHandle_t mutex_comando_motore_ralla = nullptr;
+extern SemaphoreHandle_t mutex_comando_motore_ralla;
 
-SemaphoreHandle_t mutex_comando_motore_punzone = nullptr;
+extern SemaphoreHandle_t mutex_comando_motore_punzone;
 
-SemaphoreHandle_t mutex_comando_avanti_motore_punzone = nullptr;
+extern SemaphoreHandle_t mutex_comando_avanti_motore_punzone;
 
-SemaphoreHandle_t mutex_comando_indietro_motore_punzone = nullptr;
+extern SemaphoreHandle_t mutex_comando_indietro_motore_punzone;
 
-SemaphoreHandle_t mutex_comando_avanti_motore_ralla = nullptr;
+extern SemaphoreHandle_t mutex_comando_avanti_motore_ralla;
 
-SemaphoreHandle_t mutex_comando_indietro_motore_ralla = nullptr;
+extern SemaphoreHandle_t mutex_comando_indietro_motore_ralla;
 
-SemaphoreHandle_t mutex_stato_motore_punzone = nullptr;
+extern SemaphoreHandle_t mutex_stato_motore_punzone;
 
-SemaphoreHandle_t mutex_speed_motore_punz = nullptr;
+extern SemaphoreHandle_t mutex_speed_motore_punz;
 
-SemaphoreHandle_t mutex_stato_motore_ralla = nullptr;
+extern SemaphoreHandle_t mutex_stato_motore_ralla;
 
-SemaphoreHandle_t mutex_gradi_per_click_ralla = nullptr;
+extern SemaphoreHandle_t mutex_gradi_per_click_ralla;
 
-SemaphoreHandle_t mutex_gradi_per_click_punz = nullptr;
+extern SemaphoreHandle_t mutex_gradi_per_click_punz;
 
-SemaphoreHandle_t mutex_speed_motore_ralla = nullptr;
+extern SemaphoreHandle_t mutex_speed_motore_ralla;
 
-SemaphoreHandle_t mutex_stato_sensore_di_calibrazione = nullptr;
+extern SemaphoreHandle_t mutex_stato_sensore_di_calibrazione;
 
-SemaphoreHandle_t mutex_stato_finecorsa_max = nullptr;
+extern SemaphoreHandle_t mutex_stato_finecorsa_max;
 
-SemaphoreHandle_t mutex_stato_finecorsa_min = nullptr;
+extern SemaphoreHandle_t mutex_stato_finecorsa_min;
 
-SemaphoreHandle_t mutex_homing = nullptr;
+extern SemaphoreHandle_t mutex_homing;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+/// @brief Inizializza i semafori delle variabili della UI
+/// @return Se sono stati inizializzati i mutex delle variabili della UI
+extern bool ui_init_var_mutexs();
 
-bool ui_init_var_mutexs()
-{
-    mutex_contatore_caps_totali = xSemaphoreCreateMutex();
-    if(mutex_contatore_caps_totali == nullptr)
-        return false;
 
-
-    mutex_contatore_caps_ses = xSemaphoreCreateMutex();
-    if(mutex_contatore_caps_ses == nullptr)
-        return false;
-
-
-    mutex_stato_avvio_macchina = xSemaphoreCreateMutex();
-    if(mutex_stato_avvio_macchina == nullptr)
-        return false;
-
-
-    mutex_comando_macchina = xSemaphoreCreateMutex();
-    if(mutex_comando_macchina == nullptr)
-        return false;
-
-
-    mutex_password_corretta = xSemaphoreCreateMutex();
-    if(mutex_password_corretta == nullptr)
-        return false;
-
-
-    mutex_str_logger_txt = xSemaphoreCreateMutex();
-    if(mutex_str_logger_txt == nullptr)
-        return false;
-
-
-    mutex_comando_motore_ralla = xSemaphoreCreateMutex();
-    if(mutex_comando_motore_ralla == nullptr)
-        return false;
-
-
-    mutex_comando_motore_punzone = xSemaphoreCreateMutex();
-    if(mutex_comando_motore_punzone == nullptr)
-        return false;
-
-
-    mutex_comando_avanti_motore_punzone = xSemaphoreCreateMutex();
-    if(mutex_comando_avanti_motore_punzone == nullptr)
-        return false;
-
-
-    mutex_comando_indietro_motore_punzone = xSemaphoreCreateMutex();
-    if(mutex_comando_indietro_motore_punzone == nullptr)
-        return false;
-
-
-    mutex_comando_avanti_motore_ralla = xSemaphoreCreateMutex();
-    if(mutex_comando_avanti_motore_ralla == nullptr)
-        return false;
-
-
-    mutex_comando_indietro_motore_ralla = xSemaphoreCreateMutex();
-    if(mutex_comando_indietro_motore_ralla == nullptr)
-        return false;
-
-
-    mutex_stato_motore_punzone = xSemaphoreCreateMutex();
-    if(mutex_stato_motore_punzone == nullptr)
-        return false;
-
-
-    mutex_speed_motore_punz = xSemaphoreCreateMutex();
-    if(mutex_speed_motore_punz == nullptr)
-        return false;
-
-
-    mutex_stato_motore_ralla = xSemaphoreCreateMutex();
-    if(mutex_stato_motore_ralla == nullptr)
-        return false;
-
-
-    mutex_gradi_per_click_ralla = xSemaphoreCreateMutex();
-    if(mutex_gradi_per_click_ralla == nullptr)
-        return false;
-
-
-    mutex_gradi_per_click_punz = xSemaphoreCreateMutex();
-    if(mutex_gradi_per_click_punz == nullptr)
-        return false;
-
-
-    mutex_speed_motore_ralla = xSemaphoreCreateMutex();
-    if(mutex_speed_motore_ralla == nullptr)
-        return false;
-
-
-    mutex_stato_sensore_di_calibrazione = xSemaphoreCreateMutex();
-    if(mutex_stato_sensore_di_calibrazione == nullptr)
-        return false;
-
-
-    mutex_stato_finecorsa_max = xSemaphoreCreateMutex();
-    if(mutex_stato_finecorsa_max == nullptr)
-        return false;
-
-
-    mutex_stato_finecorsa_min = xSemaphoreCreateMutex();
-    if(mutex_stato_finecorsa_min == nullptr)
-        return false;
-
-
-    mutex_homing = xSemaphoreCreateMutex();
-    if(mutex_homing == nullptr)
-        return false;
-
-        
-    return true;
+#ifdef __cplusplus
 }
+#endif
