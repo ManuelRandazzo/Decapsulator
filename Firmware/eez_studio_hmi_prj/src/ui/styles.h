f2572565c308f3d2c7cd611f5a44c505b0745c10
Manuel Randazzo <randazzomanuel05@gmail.com>
2026-06-05 18:24:54 +0200

    add(eez_studio_hmi_prj) : aggiunto l'intero progetto di EEZ Studio nel Firmware

---
+++ Firmware/eez_studio_hmi_prj/src/ui/styles.h
@@ -0,0 +1,53 @@
+#ifndef EEZ_LVGL_UI_STYLES_H
+#define EEZ_LVGL_UI_STYLES_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Style: titoli schermata iniziale
+lv_style_t *get_style_titoli_schermata_iniziale_MAIN_DEFAULT();
+void add_style_titoli_schermata_iniziale(lv_obj_t *obj);
+void remove_style_titoli_schermata_iniziale(lv_obj_t *obj);
+
+// Style: Pulsante Rosso
+lv_style_t *get_style_pulsante_rosso_MAIN_DEFAULT();
+void add_style_pulsante_rosso(lv_obj_t *obj);
+void remove_style_pulsante_rosso(lv_obj_t *obj);
+
+// Style: puls jog
+lv_style_t *get_style_puls_jog_MAIN_DEFAULT();
+lv_style_t *get_style_puls_jog_MAIN_DISABLED();
+lv_style_t *get_style_puls_jog_MAIN_PRESSED();
+void add_style_puls_jog(lv_obj_t *obj);
+void remove_style_puls_jog(lv_obj_t *obj);
+
+// Style: puls rosso verde
+lv_style_t *get_style_puls_rosso_verde_MAIN_DEFAULT();
+lv_style_t *get_style_puls_rosso_verde_MAIN_CHECKED();
+void add_style_puls_rosso_verde(lv_obj_t *obj);
+void remove_style_puls_rosso_verde(lv_obj_t *obj);
+
+// Style: Stato Sensori
+lv_style_t *get_style_stato_sensori_MAIN_DEFAULT();
+lv_style_t *get_style_stato_sensori_MAIN_CHECKED();
+void add_style_stato_sensori(lv_obj_t *obj);
+void remove_style_stato_sensori(lv_obj_t *obj);
+
+// Style: giallo
+lv_style_t *get_style_giallo_MAIN_CHECKED();
+void add_style_giallo(lv_obj_t *obj);
+void remove_style_giallo(lv_obj_t *obj);
+
+// Style: Arancione
+lv_style_t *get_style_arancione_MAIN_CHECKED();
+void add_style_arancione(lv_obj_t *obj);
+void remove_style_arancione(lv_obj_t *obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_STYLES_H*/
---
+++ Firmware/eez_studio_hmi_prj/src/ui/vars.h
@@ -0,0 +1,92 @@
+#ifndef EEZ_LVGL_UI_VARS_H
+#define EEZ_LVGL_UI_VARS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// enum declarations
+
+// Flow global variables
+
+enum FlowGlobalVariables {
+    FLOW_GLOBAL_VARIABLE_AVVIO_MACCHINA = 0,
+    FLOW_GLOBAL_VARIABLE_STATO_COMANDO_MACCHINA = 1,
+    FLOW_GLOBAL_VARIABLE_STATO_AVVIO = 2,
+    FLOW_GLOBAL_VARIABLE_PASSWORD_INSERITA = 3,
+    FLOW_GLOBAL_VARIABLE_ESITO_CONTROLLO_PSWW = 4,
+    FLOW_GLOBAL_VARIABLE_TITOLO_BOTTONE_RALLA = 5,
+    FLOW_GLOBAL_VARIABLE_TITOLO_BOTTONE_PUNZONE = 6,
+    FLOW_GLOBAL_VARIABLE_COMANDO_BOTTONE_RALLA = 7,
+    FLOW_GLOBAL_VARIABLE_COMANDO_BOTTONE_PUNZONE = 8,
+    FLOW_GLOBAL_VARIABLE_LED_MOVIMENTO_PUNZONE = 9,
+    FLOW_GLOBAL_VARIABLE_LED_MOVIMENTO_RALLA = 10,
+    FLOW_GLOBAL_VARIABLE_TITOLO_STATO_SENSORE_CALIBRAZIONE = 11,
+    FLOW_GLOBAL_VARIABLE_TITOLO_STATO_FINECORSA_MIN = 12,
+    FLOW_GLOBAL_VARIABLE_TITOLO_STATO_FINECORSA_MAX = 13,
+    FLOW_GLOBAL_VARIABLE_NOME_RETE_INSERITO = 14,
+    FLOW_GLOBAL_VARIABLE_PASSWORD_RETE_INSERITA = 15,
+    FLOW_GLOBAL_VARIABLE_MESSAGGIO_CONFIG_WI_FI = 16
+};
+
+// Native global variables
+
+extern int32_t get_var_contatore_caps_totali();
+extern void set_var_contatore_caps_totali(int32_t value);
+extern int32_t get_var_contatore_caps_ses();
+extern void set_var_contatore_caps_ses(int32_t value);
+extern bool get_var_stato_avvio_macchina();
+extern void set_var_stato_avvio_macchina(bool value);
+extern bool get_var_comando_macchina();
+extern void set_var_comando_macchina(bool value);
+extern const char *get_var_password_corretta();
+extern void set_var_password_corretta(const char *value);
+extern const char *get_var_str_logger_txt();
+extern void set_var_str_logger_txt(const char *value);
+extern bool get_var_comando_motore_ralla();
+extern void set_var_comando_motore_ralla(bool value);
+extern bool get_var_comando_motore_punzone();
+extern void set_var_comando_motore_punzone(bool value);
+extern bool get_var_comando_avanti_motore_punzone();
+extern void set_var_comando_avanti_motore_punzone(bool value);
+extern bool get_var_comando_indietro_motore_punzone();
+extern void set_var_comando_indietro_motore_punzone(bool value);
+extern bool get_var_comando_avanti_motore_ralla();
+extern void set_var_comando_avanti_motore_ralla(bool value);
+extern bool get_var_comando_indietro_motore_ralla();
+extern void set_var_comando_indietro_motore_ralla(bool value);
+extern bool get_var_stato_motore_punzone();
+extern void set_var_stato_motore_punzone(bool value);
+extern bool get_var_stato_motore_ralla();
+extern void set_var_stato_motore_ralla(bool value);
+extern const char *get_var_speed_motore_punz();
+extern void set_var_speed_motore_punz(const char *value);
+extern const char *get_var_gradi_per_click_ralla();
+extern void set_var_gradi_per_click_ralla(const char *value);
+extern const char *get_var_gradi_per_click_punz();
+extern void set_var_gradi_per_click_punz(const char *value);
+extern const char *get_var_speed_motore_ralla();
+extern void set_var_speed_motore_ralla(const char *value);
+extern bool get_var_stato_sensore_di_calibrazione();
+extern void set_var_stato_sensore_di_calibrazione(bool value);
+extern bool get_var_stato_finecorsa_max();
+extern void set_var_stato_finecorsa_max(bool value);
+extern bool get_var_stato_finecorsa_min();
+extern void set_var_stato_finecorsa_min(bool value);
+extern bool get_var_homing();
+extern void set_var_homing(bool value);
+extern bool get_var_calibrazione_touch_finita();
+extern void set_var_calibrazione_touch_finita(bool value);
+extern const char *get_var_password_rete_corretta();
+extern void set_var_password_rete_corretta(const char *value);
+extern const char *get_var_nome_rete_corretta();
+extern void set_var_nome_rete_corretta(const char *value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_VARS_H*/
