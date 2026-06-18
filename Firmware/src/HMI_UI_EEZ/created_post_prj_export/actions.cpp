1ca540274cf0497801758c64d7b35639c37205bd
Manuel Randazzo <randazzomanuel05@gmail.com>
2026-06-18 02:29:51 +0200

    refactor(touch calibration) : migliorata la calibrazione del touch, ora funziona molto meglio e non serve modificare a mano il display.
    add(jogger page init) : aggiunta l'implementazione dell'init della pagina del jogger in cui attiva la task del jogger e disattiva quella del programma principale.
    
    add(jogger page deinit) : aggiunta l'implementazione dell'init della pagina del programma principale in cui attiva la task del jogger e disattiva quella del jogger.
    
    add(refresh logs) : aggiunta una funzione che permette all'utente di refrashare i log tramite un pulsante sulla UI. Utile per non dover leggere continuamente la SD.
    
    WIP -> add(WiFi credential config) : aggiunta la configurazione del WiFi con salvataggio in SD delle credenziali.

--- Firmware/src/HMI_UI_EEZ/created_post_prj_export/actions.cpp
+++ Firmware/src/HMI_UI_EEZ/created_post_prj_export/actions.cpp
@@ -2,78 +2,156 @@
 #include "HMI_UI_EEZ/vars.h"
 #include "decapsulator_io.hpp"
 #include "HMI_UI_EEZ/screens.h"
+#include "tasks_cfg.hpp"
+#include "filePathsSD.hpp"
+#include "WiFi_Config.hpp"
+#include "Debug.hpp"
 
 void action_calibrazione_touch(lv_event_t *e)
 {
+    /// Refresh forzato
+    lv_obj_invalidate(lv_screen_active());
+    lv_refr_now(lv_obj_get_display(lv_screen_active()));
+
     /// Comunica alla UI che faccia la calibrazione del touch
     set_var_calibrazione_touch_finita(false);
 
     /// Background color
     constexpr uint32_t BG_COLOR = TFT_BLACK;
+
     /// Foreground color
     constexpr uint32_t FG_COLOR = TFT_RED;
 
-    uint16_t calib_datas[5] = { 0, 0, 0, 0, 0 };
-    
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_black(), LV_PART_MAIN);
+    uint16_t calib_datas[5] = { 0, 0, 0, 0, 0 };    
 
-    lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, LV_PART_MAIN);
-
-    /// Crea la label
-    lv_obj_t * label = lv_label_create(lv_screen_active());
-
-    /// Imposta il colore del testo a bianco
-    lv_obj_set_style_text_color(label, lv_color_hex(0xffffff), LV_PART_MAIN); 
-    
-    /// Centra la label
-    lv_obj_center(label);
-
-    /// Setta il font
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_PART_MAIN);
-
-    /// Imposta il testo
-    lv_label_set_text(label, "                  Calibrazione del display.\
-                            \n                  Si consiglia di utilizzare un pennino.\
-                            \n                  Toccare gli angoli indicati dalla freccia\
-                            \n                  nella sequenza richiesta.\
-                            \n                  Prendersi tutto il tempo necessario");
-    
-    /// Refresh forzato
-    lv_obj_invalidate(lv_screen_active());
-    lv_refr_now(lv_obj_get_display(lv_screen_active())); 
-    
     tft.calibrateTouch(calib_datas, FG_COLOR, BG_COLOR, 40);
-    tft.setTouch(calib_datas);
-
-    /// Scrive i dati di calibrazione nella SD
-    for(uint8_t i=0; i < 5; i++)
-        SD_Card.setValueByKey("/touch_calibration.txt", "data[" + String(i) + "]", calib_datas[i]);
 
     /// Comunica alla UI che la calibrazione del touch è terminata
     set_var_calibrazione_touch_finita(true);
 
-    /// Elimina la label
-    lv_obj_delete(label);
-    
-    /// Refresh forzato
-    lv_obj_invalidate(lv_screen_active());
-    lv_refr_now(lv_obj_get_display(lv_screen_active())); 
-};
+    /// Imposta i dati di calibrazione ottenuti
+    tft.setTouch(calib_datas);
 
-void action_logger_page_init(lv_event_t * e){
-};
+    /// Scrive i dati di calibrazione nella SD
+    for(uint8_t i=0; i < 5; i++)
+        SD_Card.setValueByKey(HMI_TOUCH_CALIB_PATH_SD, "data[" + String(i) + "]", calib_datas[i]);
+}
 
-void action_logger_page_deinit(lv_event_t * e){
-};
 
-void action_jogger_page_init(lv_event_t * e){
-};
 
-void action_jogger_page_deinit(lv_event_t * e){
-};
 
-void action_conf_wi_fi_page_init(lv_event_t * e){
-};
+void action_logger_page_init(lv_event_t * e)
+{
+    /// Legge i log dalla microSD e li carica nella variabile della UI
+    set_var_str_logger_txt(SD_Card.readFile(LOG_PATH_SD).c_str());
+}
 
-void action_conf_wi_fi_page_deinit(lv_event_t * e){
-};
+
+
+
+void action_logger_page_deinit(lv_event_t * e)
+{
+
+}
+
+
+
+
+void action_jogger_page_init(lv_event_t * e)
+{
+    /// Sospende la task del programma principale e attiva quella del jogger
+    vTaskSuspend(MainPrgHandler);
+    vTaskResume(JogMotoriPrgHandler);
+
+    /// Inizializza lo spazio percorso e la velocità dei motori di default
+    set_var_gradi_per_click_ralla("360.0");
+    set_var_speed_motore_ralla("90.0");
+    set_var_gradi_per_click_punz("360.0");
+    set_var_speed_motore_punz("90.0");
+
+    /// Si assicura che sia accesa la ventola
+    Ventola.on();
+}
+
+
+
+
+void action_jogger_page_deinit(lv_event_t * e)
+{
+    /// Sospende la task del jogger e attiva quella del programma principale
+    vTaskSuspend(JogMotoriPrgHandler);
+    vTaskResume(MainPrgHandler);
+
+    /// Si assicura che sia spenta la ventola
+    Ventola.off();
+}
+
+
+
+
+void action_conf_wi_fi_page_init(lv_event_t * e)
+{
+}
+
+
+
+
+void action_conf_wi_fi_page_deinit(lv_event_t * e)
+{
+
+}
+
+
+
+
+void action_refresh_logs(lv_event_t * e)
+{
+    /// Legge i log dalla microSD e li carica nella variabile della UI
+    set_var_str_logger_txt(SD_Card.readFile(LOG_PATH_SD).c_str());
+}
+
+
+
+
+void action_verify_wi_fi(lv_event_t * e)
+{
+    /// Salva in SD il nuovo SSID e la nuova Password
+    /** @todo : 
+     *          1. Controllare se è possibile connettersi
+     *             con le nuove credenziali.
+     *          2. Restituire un bool alla UI -> true=connesso, false=credenziali errate.
+     *          3. se è true salva le credenziali in microSD
+     */
+
+    WiFi.mode(WIFI_STA); //per evitare conflitti con le risorse del bluetooth
+
+    String tmpSSID = ""; // Mettere get_var_nome_rete_inserito();
+    String tmpPASS = get_var_password_rete_inserita();
+
+    WiFi.begin(tmpSSID, tmpPASS);
+    if(WiFi.waitForConnectResult(DEFAULT_TIMEOUT_WIFI_CONNECTION_IN_MS) != WL_CONNECTED)
+    {
+        if(WIFI_SSID != "")
+        {
+            /// Prova a riconnettersi alla rete di prima
+            WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+            if(WiFi.waitForConnectResult(DEFAULT_TIMEOUT_WIFI_CONNECTION_IN_MS) != WL_CONNECTED)
+                return;
+        }
+    }
+
+    /// Assegna le nuovi credenziali (se sono cambiate)
+    if(tmpSSID != WIFI_SSID )
+    {
+        WIFI_SSID = tmpSSID;
+        SD_Card.setValueByKey(WIFI_PATH_SD, "SSID", WIFI_SSID, "Nome della rete dell'utente");
+    }
+
+    if(tmpPASS != WIFI_PASSWORD)
+    {
+        WIFI_PASSWORD = tmpPASS;
+        SD_Card.setValueByKey(WIFI_PATH_SD, "WIFI_PASSWORD", WIFI_PASSWORD, "Password della rete dell'utente");
+    }
+
+    LogInfo("Credenziali WiFi", "Cambiate le credenziali del WiFi");
+}
